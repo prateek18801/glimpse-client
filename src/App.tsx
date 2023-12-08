6fbfc2800c14935e492274b3196ca0123df8c00d
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-09 01:22:10 +0530

    Setup basic page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
     return (
-        <>
-            <h1 className="text-lg font-bold text-center">Hello World</h1>
-        </>
+        <Routes>
+            <Route path="/" element={<>Home</>} />
+            <Route path="/about" element={<>About</>} />
+            <Route path="/contact" element={<>Contact</>} />
+            <Route path="/services" element={<>Services</>} />
+            <Route path="/login" element={<>Login</>} />
+            <Route path="/signup" element={<>Signup</>} />
+            <Route path="/dashboard" element={<>Dashboard</>} />
+        </Routes>
     );
 }
 
