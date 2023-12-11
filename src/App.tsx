fadf9f54d3fa0a5c972d3fe3368a86022c47f6d3
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-11 22:55:55 +0530

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
     return (
         <Routes>
-            <Route path="/" element={<>Home</>} />
+            <Route path="/" element={<Home />} />
             <Route path="/about" element={<>About</>} />
             <Route path="/contact" element={<>Contact</>} />
             <Route path="/services" element={<>Services</>} />
