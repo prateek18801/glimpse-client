3e8d0874d1dfba3bb03d75e0c53c384f0a4fead7
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-16 18:55:25 +0530

    Link to contact section

--- src/pages/Home/Contact.tsx
+++ src/pages/Home/Contact.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Contact = () => {
     return (
         <section className="py-20 mb-20 bg-black">
             <div className="mb-2 text-2xl font-semibold text-center text-white">Take the First Step Towards Printing Success</div>
             <div className="mb-12 text-center text-stone-300">Elevate your marketing needs to new levels!</div>
             <div className="flex justify-center">
-                <button className="py-3 font-medium bg-white rounded-sm px-7">Contact Sales</button>
+                <Link to="/contact">
+                    <button className="py-3 font-medium bg-white rounded-sm px-7">Contact Sales</button>
+                </Link>
             </div>
         </section>
     );
