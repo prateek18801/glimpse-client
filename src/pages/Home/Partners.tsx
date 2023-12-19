b008bc4b51ebd1c9e3c583ddb037f6d57fe048fe
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-19 12:01:32 +0530

    Update partner logo styling

--- src/pages/Home/Partners.tsx
+++ src/pages/Home/Partners.tsx
@@ -7,12 +7,12 @@ const Partners = () => {
             <div className="mb-12 text-2xl font-semibold text-center">Brand Partners</div>
             <div className="flex gap-4 px-8">
                 <a href="https://4excelerate.org/" target="_blank">
-                    <div className="bg-white shadow rounded-sm h-full items-center justify-center flex p-4">
+                    <div className="flex items-center justify-center h-full bg-white rounded-sm">
                         <img src={ExcelerateLogo} alt="excelerate" />
                     </div>
                 </a>
                 <a href="https://globallyrecruit.net/" target="_blank">
-                    <div className="bg-white shadow rounded-sm h-full items-center justify-center flex p-4">
+                    <div className="flex items-center justify-center h-full bg-white rounded-sm">
                         <img src={GlobRecruitLogo} alt="globally recruit" />
                     </div>
                 </a>
