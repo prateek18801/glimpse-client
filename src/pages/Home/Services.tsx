df71d6b87cf180cfa13c0d9555e9064d0e45fecb
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-21 03:59:15 +0530

    Fix service section responsiveness add links to service page

--- src/pages/Home/Services.tsx
+++ src/pages/Home/Services.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Services = () => {
 
     const services = [
@@ -21,7 +23,7 @@ const Services = () => {
     return (
         <section className="py-10 mb-20 bg-black">
             <div className="mb-12 text-2xl font-semibold text-center text-white">Services We Offer</div>
-            <div className="flex flex-wrap px-8">
+            <div className="flex flex-wrap justify-center gap-16 px-8 mb-10">
                 {services.map((service, i) => {
                     return <ServiceCard key={i} {...service} />;
                 })}
@@ -32,14 +34,16 @@ const Services = () => {
 
 const ServiceCard = ({ title, backdrop, description }: { title: string, backdrop: string, description: string }) => {
     return (
-        <div className="mb-10 overflow-hidden bg-white rounded-sm shadow">
-            <div className="bg-gray-400 w-full aspect-square relative">
-                <img className="shadow" src={backdrop} alt={title} />
-                <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center text-2xl font-medium" style={{backgroundColor: "rgba(0,0,0,0.2)"}}>{title}</div>
-            </div>
-            <div className="p-4">
-                {description}
-            </div>
+        <div className="overflow-hidden bg-white rounded-sm shadow md:w-1/4 group ">
+            <Link to="/services">
+                <div className="relative w-full overflow-hidden bg-gray-400 h-72">
+                    <img className="absolute duration-300 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 group-hover:scale-105 group-hover:transition-transform" src={backdrop} alt={title} />
+                    <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full text-2xl font-medium" style={{ backgroundColor: "rgba(0,0,0,0.2)" }}>{title}</div>
+                </div>
+                <div className="p-4">
+                    {description}
+                </div>
+            </Link>
         </div>
     );
 }
