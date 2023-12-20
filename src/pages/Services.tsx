4914ca3dd4ca294fb3a7053c3f5edc089f835752
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-21 04:03:04 +0530

    Make services page responsive

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -27,7 +27,7 @@ const Services = () => {
             <Navbar />
             <section className="my-32">
                 <div className="mb-12 text-2xl font-semibold text-center">What We Do</div>
-                <div className="flex flex-wrap gap-10 px-8">
+                <div className="flex flex-wrap justify-center gap-10 px-8">
                     {services.map((service, i) => {
                         return <ServiceCard key={i} {...service} />;
                     })}
@@ -40,14 +40,14 @@ const Services = () => {
 
 const ServiceCard = ({ title, image, description }: { title: string, image: string, description: string }) => {
     return (
-        <div className="overflow-hidden bg-white rounded-sm shadow">
+        <div className="overflow-hidden bg-white rounded-sm shadow md:w-1/4">
             <div className="w-full mb-4 bg-gray-400 aspect-square">
                 <img src={image} alt={title} />
             </div>
             <div className="mx-4 mb-2 text-2xl font-medium text-center">{title}</div>
             <div className="mx-4 mb-4 text-center">{description}</div>
             <div className="mx-4 mb-8 text-center">
-                <Link to="/contact" state={{subject: `Query: ${title}`}}>
+                <Link to="/contact" state={{subject: `Service: ${title}`}}>
                     <button className="p-4 font-medium text-white bg-black rounded-sm">Contact Sales</button>
                 </Link>
             </div>
