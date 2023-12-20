4897dbd79d5ab8d4ef8541b049cace80b55689f6
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-21 03:57:46 +0530

    Implement responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,28 +21,50 @@ const Navbar = () => {
 
     return (
         <nav ref={navRef}>
-            <div className="fixed top-0 left-0 z-30 flex items-center justify-center w-screen py-8 bg-white shadow">
-                <button className="absolute p-2 text-2xl left-6" onClick={() => setIsExpanded(prev => !prev)}>
-                    <IoMdMenu />
-                </button>
-                <div className="text-xl font-logo">GLIMPSE</div>
+            <div className="md:hidden">
+                <div className="fixed top-0 left-0 z-30 flex items-center justify-center w-screen py-8 bg-white shadow">
+                    <button className="absolute p-2 text-2xl left-6" onClick={() => setIsExpanded(prev => !prev)}>
+                        <IoMdMenu />
+                    </button>
+                    <div className="text-xl font-logo">GLIMPSE</div>
+                </div>
+                <div className={`fixed top-0 left-0 h-screen z-20 pl-8 transition-transform bg-white w-60 ${isExpanded ? "" : "-translate-x-60"}`}>
+                    <ul className="mt-32 font-medium">
+                        <li className="mb-6">
+                            <NavLink to="/" className={({ isActive }) => isActive ? "underline underline-offset-8" : ""}>HOME</NavLink>
+                        </li>
+                        <li className="mb-6">
+                            <NavLink to="/services" className={({ isActive }) => isActive ? "underline underline-offset-8" : ""}>SERVICES</NavLink>
+                        </li>
+                        <li className="mb-6">
+                            <NavLink to="/about" className={({ isActive }) => isActive ? "underline underline-offset-8" : ""}>ABOUT</NavLink>
+                        </li>
+                        <li className="mb-12">
+                            <NavLink to="/contact" className={({ isActive }) => isActive ? "underline underline-offset-8" : ""}>CONTACT</NavLink>
+                        </li>
+                        <NavLink to="/login"><button className="px-4 py-2 text-white bg-black rounded-sm">LOGIN</button></NavLink>
+                    </ul>
+                </div>
             </div>
-            <div className={`fixed top-0 left-0 h-screen z-20 pl-8 transition-transform bg-white w-60 ${isExpanded ? "" : "-translate-x-60"}`}>
-                <ul className="mt-32 font-medium">
-                    <li className="mb-6">
-                        <NavLink to="/" className={({ isActive }) => isActive ? "underline underline-offset-8" : ""}>HOME</NavLink>
-                    </li>
-                    <li className="mb-6">
-                        <NavLink to="/services" className={({ isActive }) => isActive ? "underline underline-offset-8" : ""}>SERVICES</NavLink>
-                    </li>
-                    <li className="mb-6">
-                        <NavLink to="/about" className={({ isActive }) => isActive ? "underline underline-offset-8" : ""}>ABOUT</NavLink>
-                    </li>
-                    <li className="mb-12">
-                        <NavLink to="/contact" className={({ isActive }) => isActive ? "underline underline-offset-8" : ""}>CONTACT</NavLink>
-                    </li>
-                    <NavLink to="/login"><button className="px-4 py-2 text-white bg-black">LOGIN</button></NavLink>
-                </ul>
+            <div className="fixed top-0 left-0 z-30 items-center justify-between hidden w-full px-16 py-6 bg-white shadow-sm md:flex">
+                <div className="flex gap-x-16">
+                    <div className="text-2xl font-logo">GLIMPSE</div>
+                    <ul className="flex items-center gap-4 text-sm font-medium text-neutral-600">
+                        <li className="">
+                            <NavLink to="/" className={({ isActive }) => isActive ? "underline underline-offset-8 text-black" : ""}>HOME</NavLink>
+                        </li>
+                        <li className="">
+                            <NavLink to="/services" className={({ isActive }) => isActive ? "underline underline-offset-8 text-black" : ""}>SERVICES</NavLink>
+                        </li>
+                        <li className="">
+                            <NavLink to="/about" className={({ isActive }) => isActive ? "underline underline-offset-8 text-black" : ""}>ABOUT</NavLink>
+                        </li>
+                        <li className="">
+                            <NavLink to="/contact" className={({ isActive }) => isActive ? "underline underline-offset-8 text-black" : ""}>CONTACT</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <NavLink to="/login"><button className="px-6 py-3 text-sm font-medium text-white bg-black rounded-sm">LOGIN</button></NavLink>
             </div>
         </nav>
     );
