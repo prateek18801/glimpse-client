5cc2b99679b2916186cb24696f5a0302e575a369
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-21 04:00:24 +0530

    Fix responsiveness add links to contact page

--- src/pages/Home/Printing.tsx
+++ src/pages/Home/Printing.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Printing = () => {
     const prints = [
         {
@@ -33,9 +35,9 @@ const Printing = () => {
     ];
 
     return (
-        <section className="mb-24">
+        <section className="px-4 mb-24 md:px-16">
             <div className="mb-12 text-2xl font-semibold text-center">Popular Printing Categories</div>
-            <div className="grid grid-cols-2 gap-4 px-4">
+            <div className="grid grid-cols-2 gap-4 md:grid-cols-6">
                 {prints.map((print, i) => {
                     return <PrintingCard key={i} {...print} />;
                 })}
@@ -47,13 +49,15 @@ const Printing = () => {
 const PrintingCard = ({ image, title, description }: { image: string, title: string, description: string }) => {
     return (
         <div className="bg-white rounded-sm shadow">
-            <div className="bg-gray-400 aspect-square">
-                <img src={image} alt={title} className="shadow-sm" />
-            </div>
-            <div className="p-4">
-                <div className="font-semibold">{title}</div>
-                <div className="text-xs text-gray-600">{description}</div>
-            </div>
+            <Link to="/contact" state={{ subject: `Product: ${title}` }}>
+                <div className="bg-gray-400 aspect-square">
+                    <img src={image} alt={title} className="shadow-sm" />
+                </div>
+                <div className="p-4">
+                    <div className="font-semibold">{title}</div>
+                    <div className="text-xs text-gray-600">{description}</div>
+                </div>
+            </Link>
         </div>
     );
 }
