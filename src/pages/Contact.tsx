8b6643b6ad7f8e715ca7b8e90d6356d68142df51
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-21 13:00:41 +0530

    Fix responsiveness Update error message

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -15,7 +15,7 @@ type FormDataType = {
 };
 
 const Contact = () => {
-    const location = useLocation();    
+    const location = useLocation();
 
     const [formData, setFormData] = useState<FormDataType>({
         name: "",
@@ -48,10 +48,10 @@ const Contact = () => {
                 });
             }
             else {
-                alert("Failed to send message!");
+                alert("Failed to send message. Some error occured :(");
             }
         } catch (err) {
-            alert("Failed to send message!");
+            alert("Failed to send message. Some error occured :(");
         } finally {
             setLoading(false);
         }
@@ -69,35 +69,36 @@ const Contact = () => {
     return (
         <>
             <Navbar />
-            <section className="my-32">
-                <div className="mb-12 text-2xl font-semibold text-center">Contact Us</div>
-                <form className="w-full px-8" onSubmit={handleFormSubmit}>
-                    <div className="contact-form-control">
-                        <input type="text" name="name" id="name" className="contact-input peer" value={formData.name} onChange={handleInputChange} placeholder=" " required />
-                        <label htmlFor="name" className="contact-label">Name</label>
-                    </div>
-                    <div className="contact-form-control">
-                        <input type="email" name="email" id="email" className="contact-input peer" value={formData.email} onChange={handleInputChange} placeholder=" " required />
-                        <label htmlFor="email" className="contact-label">Email</label>
-                    </div>
-                    <div className="contact-form-control">
-                        <input type="tel" name="contact" id="contact" className="contact-input peer" value={formData.contact} onChange={handleInputChange} placeholder=" " />
-                        <label htmlFor="contact" className="contact-label">Contact</label>
-                    </div>
-                    <div className="contact-form-control">
-                        <input type="text" name="subject" id="subject" className="contact-input peer" value={formData.subject} onChange={handleInputChange} placeholder=" " required />
-                        <label htmlFor="subject" className="contact-label">Subject</label>
-                    </div>
-
-                    <div className="contact-form-control !h-36">
-                        <textarea name="message" id="message" className="py-4 resize-none contact-input peer" value={formData.message} onChange={handleInputChange} placeholder=" " />
-                        <label htmlFor="message" className="contact-label">Message</label>
-                    </div>
-                    <button className="flex items-center justify-center px-8 py-4 font-medium text-white bg-black rounded-sm" type="submit" disabled={loading} aria-label="submit">
-                        <span className="mr-3">Submit</span>
-                        {loading ? <AiOutlineLoading3Quarters className="animate-spin" /> : <PiPaperPlaneTiltBold />}
-                    </button>
-                </form>
+            <section className="py-32">
+                <div className="mb-12 text-2xl font-semibold text-center md:text-3xl">Contact Us</div>
+                <div className="flex justify-center">
+                    <form className="w-full max-w-2xl px-8" onSubmit={handleFormSubmit}>
+                        <div className="contact-form-control">
+                            <input type="text" name="name" id="name" className="contact-input peer" value={formData.name} onChange={handleInputChange} placeholder=" " required />
+                            <label htmlFor="name" className="contact-label">Name</label>
+                        </div>
+                        <div className="contact-form-control">
+                            <input type="email" name="email" id="email" className="contact-input peer" value={formData.email} onChange={handleInputChange} placeholder=" " required />
+                            <label htmlFor="email" className="contact-label">Email</label>
+                        </div>
+                        <div className="contact-form-control">
+                            <input type="tel" name="contact" id="contact" className="contact-input peer" value={formData.contact} onChange={handleInputChange} placeholder=" " />
+                            <label htmlFor="contact" className="contact-label">Contact</label>
+                        </div>
+                        <div className="contact-form-control">
+                            <input type="text" name="subject" id="subject" className="contact-input peer" value={formData.subject} onChange={handleInputChange} placeholder=" " required />
+                            <label htmlFor="subject" className="contact-label">Subject</label>
+                        </div>
+                        <div className="contact-form-control !h-36">
+                            <textarea name="message" id="message" className="py-4 resize-none contact-input peer" value={formData.message} onChange={handleInputChange} placeholder=" " />
+                            <label htmlFor="message" className="contact-label">Message</label>
+                        </div>
+                        <button className="flex items-center justify-center px-8 py-4 font-medium text-white bg-black rounded-sm" type="submit" disabled={loading} aria-label="submit">
+                            <span className="mr-3">Submit</span>
+                            {loading ? <AiOutlineLoading3Quarters className="animate-spin" /> : <PiPaperPlaneTiltBold />}
+                        </button>
+                    </form>
+                </div>
             </section>
             <Footer />
         </>
