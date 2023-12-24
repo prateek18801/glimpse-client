47ba849d253af8ec8f33504251b9fd374b342c73
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-24 12:08:39 +0530

    Implement loading remove unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
+import { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
+import ConfigContext from "./contexts/ConfigContext";
 
 const App = () => {
+
+    const config = useContext(ConfigContext);
+    const loading = config.loading;    
+
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/login" element={<>Login</>} />
-            <Route path="/signup" element={<>Signup</>} />
-            <Route path="/dashboard" element={<>Dashboard</>} />
-        </Routes>
+        loading ?
+            <h1>Loading Glimpse</h1>
+            :
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/services" element={<Services />} />
+            </Routes>
     );
 }
 
