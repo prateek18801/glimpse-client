ba9a27e1195977fab262136ebf020c033d141be9
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-24 11:59:47 +0530

    Wrap app in config provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { ConfigProvider } from "./contexts/ConfigContext.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <ConfigProvider>
+                <App />
+            </ConfigProvider>
         </BrowserRouter>
     </React.StrictMode>,
 );
