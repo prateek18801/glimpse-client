33ab2028a375aa0f332798ef4683ca357c4b921f
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-25 02:47:47 +0530

    Add data request to backend and add loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Loading from "./pages/Loading";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
 import ConfigContext from "./contexts/ConfigContext";
+import useFetch from "./hooks/useFetch";
+import ConfigDataType, { defaultConfig } from "./config";
 
 const App = () => {
 
-    const config = useContext(ConfigContext);
-    const loading = config.loading;    
+    const { setData } = useContext(ConfigContext);
+    const { response, status } = useFetch<ConfigDataType>("/v1/config", defaultConfig);
+
+    useEffect(() => {
+        setData(response);
+    }, [response, status]);
 
     return (
-        loading ?
-            <h1>Loading Glimpse</h1>
-            :
+        status === "success" ?
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/services" element={<Services />} />
             </Routes>
+            :
+            <Loading status={status} />
     );
 }
 
