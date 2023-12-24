7800c829120c19077952e5a24daacfe9bfd3a891
Prateek Chaurasia <prateek18801@gmail.com>
2023-12-25 02:44:13 +0530

    Remove api call from context

--- src/contexts/ConfigContext.tsx
+++ src/contexts/ConfigContext.tsx
@@ -1,26 +1,16 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import ConfigDataType, { defaultConfig } from "../config";
-import useFetch from "../hooks/useFetch";
 
-const ConfigContext = createContext<{ data: ConfigDataType, loading: boolean }>({
+const ConfigContext = createContext<{ data: ConfigDataType, setData: React.Dispatch<React.SetStateAction<ConfigDataType>> }>({
     data: defaultConfig,
-    loading: true
+    setData: () => { }
 });
 
 export const ConfigProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
     const [data, setData] = useState<ConfigDataType>(defaultConfig);
-
-    const { response, loading, error } = useFetch<ConfigDataType>("/api/v1/config", defaultConfig);
-
-    useEffect(() => {
-        setData(() => {
-            return error ? defaultConfig : response;
-        });
-    }, [response, error]);
-
     return (
-        <ConfigContext.Provider value={{ data, loading }}>
+        <ConfigContext.Provider value={{ data, setData }}>
             {children}
         </ConfigContext.Provider>
     );
