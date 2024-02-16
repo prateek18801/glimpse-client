b764ee2568562b0cd2948e6a5521781f70fa5796
Prateek Chaurasia <prateek18801@gmail.com>
2024-02-17 02:06:43 +0530

    Implement page layout remove config fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useContext, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Loading from "./pages/Loading";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
-import ConfigContext from "./contexts/ConfigContext";
-import useFetch from "./hooks/useFetch";
-import ConfigDataType, { sampleConfig } from "./config";
+import PageLayout from "./components/PageLayout";
 
 const App = () => {
-
-    const { setData } = useContext(ConfigContext);
-    const { response, status } = useFetch<ConfigDataType>("/v1/config", sampleConfig);
-
-    useEffect(() => {
-        setData(response);
-    }, [response, status]);
-
     return (
-        status === "success" ?
-            <Routes>
+        <Routes>
+            <Route element={<PageLayout />}>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/services" element={<Services />} />
-            </Routes>
-            :
-            <Loading status={status} />
+            </Route>
+        </Routes>
     );
 }
 
