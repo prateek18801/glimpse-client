42f96a9e5202911eca654939795fe4f7ca7945f2
Prateek Chaurasia <prateek18801@gmail.com>
2024-02-17 12:48:32 +0530

    Add sample website config

--- src/config.ts
+++ src/config.ts
@@ -1,62 +1,125 @@
-type CardDataType = {
-    image: string,
-    title: string,
-    description: string
-}
-
-type ConfigDataType = {
-    services: CardDataType[],
-    about: CardDataType[],
-    printing: CardDataType[],
-    partners: string[],
-    reviews: {
-        rating: number,
-        author: string,
-        institute: string,
-        description: string
-    }[],
-    socials: {
-        instagram: string,
-        email: string,
-        whatsapp: string,
-        linkedin: string
-    },
-    carousel: string[],
-    loginUrl: string
-};
-
-export const sampleConfig: ConfigDataType = {
+const config: TConfig = {
     services: [
         {
-            title: "",
-            image: "",
-            description: ""
+            title: "Direct Print Mail",
+            image: "https://drive.google.com/uc?id=14oXQUTWrxlZH3D0QRQCEi16MGrlvmgDR",
+            description: "Delivering communication solutions, ensuring your messages reach the intended recipients seamlessly and with maximum impact."
+        },
+        {
+            title: "Merchandise For Fairs",
+            image: "https://drive.google.com/uc?id=1qW3eeu7dyQJqXGY2ja7ysD_fJqLYQh_E",
+            description: "Creating customized merchandise that embodies your brand identity, captivates your target audience, and leaves a lasting impression."
+        },
+        {
+            title: "Welcome Kit Programs",
+            image: "https://drive.google.com/uc?id=1nQUlTeEW8472B3KaxQoxaDqclDZGR8tn",
+            description: "Managing exceptional welcome kits that foster a sense of belonging and appreciation among new members."
         }
     ],
     about: [
         {
-            title: "",
-            image: "",
-            description: ""
-        }
-    ],
-    printing: [
+            title: "Specializes",
+            image: "https://drive.google.com/uc?id=1TNC8nbOuOrJ4BH1S4ZlqujmoMAC3twXI",
+            description: "Glimpse Education Private Limited specializes in providing comprehensive services in the field of direct print mail, merchandise for fairs, and welcome kits."
+        },
         {
-            title: "",
+            title: "Commitment",
+            image: "https://drive.google.com/uc?id=1xQqUddElEfjSw_Rfe_xFtCSTlBiJa8RC",
+            description: "Their commitment to excellence and attention to detail has established them as a trusted partner for organizations seeking superior solutions for their promotional needs."
+        },
+        {
+            title: "Tailored solutions",
             image: "",
-            description: ""
+            description: "We offer tailored solutions for designing, printing, and distributing promotional materials that cater to the unique requirements of each client."
+        },
+        {
+            title: "Customized merchandise",
+            image: "",
+            description: "Glimpse Education Private Limited provides customized merchandise for fairs and events that embodies their clients' brand identity, captivates their target audience, and leaves a lasting impression."
+        },
+        {
+            title: "Expertise",
+            image: "https://drive.google.com/uc?id=1CBuhS15LGkp8gQ2a6T5tnaZmtFB6a7xg",
+            description: "The company has expertise in managing welcome kit programs and has successfully partnered with esteemed institutions such as Saint Louis University to create exceptional welcome kits."
+        },
+        {
+            title: "Value",
+            image: "https://drive.google.com/uc?id=1WW4TLgwdFZp7PZ5YHfqjWac8JoLFtOCt",
+            description: "We value professionalism, innovation, and client satisfaction above all else and have forged strong partnerships with globally renowned organizations such as Globally Recruit, Globalshala, and Excelerate."
+        },
+        {
+            title: "Dedicated team",
+            image: "https://drive.google.com/uc?id=1uhqL6WMRW94Dw58UQgpUWUjwDzvu3YGq",
+            description: "Glimpse Education Private Limited's dedicated team of experts is always ready to go the extra mile to ensure that clients' expectations are exceeded."
+        },
+        {
+            title: "Timely delivery",
+            image: "https://drive.google.com/uc?id=1WFObDAjlJsrRn27s3dI-BVbIkGryze5n",
+            description: "Their commitment to excellence, tailored solutions, and timely delivery sets them apart in the industry and guarantees a remarkable experience for their clients."
         }
     ],
-    partners: [
-        "",
-        ""
+    printCategories: [
+        {
+            title: "Paper Bags",
+            image: "https://image.made-in-china.com/202f0j00zQnoVGjBYfbp/Hot-Sale-Cheap-Colorful-Eco-Friendly-Paper-Bag-Printing-for-Clothes-Elegant-Gift-Goodies-Color-Packaging-Paper-Bag.webp",
+            description: "lorem ipsum dolor sit amet"
+        },
+        {
+            title: "Paper Bags",
+            image: "https://image.made-in-china.com/202f0j00zQnoVGjBYfbp/Hot-Sale-Cheap-Colorful-Eco-Friendly-Paper-Bag-Printing-for-Clothes-Elegant-Gift-Goodies-Color-Packaging-Paper-Bag.webp",
+            description: "lorem ipsum dolor sit amet"
+        },
+        {
+            title: "Paper Bags",
+            image: "https://image.made-in-china.com/202f0j00zQnoVGjBYfbp/Hot-Sale-Cheap-Colorful-Eco-Friendly-Paper-Bag-Printing-for-Clothes-Elegant-Gift-Goodies-Color-Packaging-Paper-Bag.webp",
+            description: "lorem ipsum dolor sit amet"
+        },
+        {
+            title: "Paper Bags",
+            image: "https://image.made-in-china.com/202f0j00zQnoVGjBYfbp/Hot-Sale-Cheap-Colorful-Eco-Friendly-Paper-Bag-Printing-for-Clothes-Elegant-Gift-Goodies-Color-Packaging-Paper-Bag.webp",
+            description: "lorem ipsum dolor sit amet"
+        },
+        {
+            title: "Paper Bags",
+            image: "https://image.made-in-china.com/202f0j00zQnoVGjBYfbp/Hot-Sale-Cheap-Colorful-Eco-Friendly-Paper-Bag-Printing-for-Clothes-Elegant-Gift-Goodies-Color-Packaging-Paper-Bag.webp",
+            description: "lorem ipsum dolor sit amet"
+        },
+        {
+            title: "Paper Bags",
+            image: "https://image.made-in-china.com/202f0j00zQnoVGjBYfbp/Hot-Sale-Cheap-Colorful-Eco-Friendly-Paper-Bag-Printing-for-Clothes-Elegant-Gift-Goodies-Color-Packaging-Paper-Bag.webp",
+            description: "lorem ipsum dolor sit amet"
+        }
     ],
     reviews: [
         {
-            description: "",
-            author: "",
-            institute: "",
+            description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum incidunt, recusandae voluptates at perspiciatis ipsam culpa eius in accusamus eveniet, vitae tempora explicabo harum quae.",
+            author: "Prateek Chaurasia",
+            institute: "Ajay Kumar Garg Engineering College",
+            rating: 4
+        },
+        {
+            description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum incidunt, recusandae voluptates at perspiciatis ipsam culpa eius in accusamus eveniet, vitae tempora explicabo harum quae.",
+            author: "Prateek Chaurasia",
+            institute: "Ajay Kumar Garg Engineering College",
             rating: 5
+        },
+        {
+            description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum incidunt, recusandae voluptates at perspiciatis ipsam culpa eius in accusamus eveniet, vitae tempora explicabo harum quae.",
+            author: "Prateek Chaurasia",
+            institute: "Ajay Kumar Garg Engineering College",
+            rating: 3
+        },
+        {
+            description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum incidunt, recusandae voluptates at perspiciatis ipsam culpa eius in accusamus eveniet, vitae tempora explicabo harum quae.",
+            author: "Prateek Chaurasia",
+            institute: "Ajay Kumar Garg Engineering College",
+            rating: 5
+        },
+        {
+            description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum incidunt, recusandae voluptates at perspiciatis ipsam culpa eius in accusamus eveniet, vitae tempora explicabo harum quae.",
+            author: "Prateek Chaurasia",
+            institute: "Ajay Kumar Garg Engineering College",
+            rating: 2
         }
     ],
     socials: {
@@ -65,11 +128,7 @@ export const sampleConfig: ConfigDataType = {
         whatsapp: "",
         linkedin: ""
     },
-    carousel: [
-        "",
-        ""
-    ],
-    loginUrl: ""
+    loginRedirectUrl: ""
 }
 
-export default ConfigDataType;
+export default config;
