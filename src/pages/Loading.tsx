56bf4a65ca78c701280447185381ecff08bc5665
Prateek Chaurasia <prateek18801@gmail.com>
2024-02-17 23:14:43 +0530

    Update loading page props

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,10 +1,14 @@
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
-const Loading = ({ status }: { status: "error" | "loading" | "success" }) => {
+const Loading = ({ error }: { error: boolean }) => {
     return (
         <section className="flex flex-col items-center justify-center w-screen h-screen text-white bg-black">
             <div className="mb-10 text-5xl select-none font-logo md:text-6xl">GLIMPSE</div>
-            {status === "error" ? <div className="px-8 text-center text-neutral-300">We are facing some trouble at the moment, please try again shortly!</div> : <div className="text-2xl md:text-3xl animate-spin"><AiOutlineLoading3Quarters /></div>}
+            {error ?
+                <div className="px-8 text-center text-neutral-300">We are facing some trouble at the moment, please try again shortly!</div>
+                :
+                <div className="text-2xl md:text-3xl animate-spin"><AiOutlineLoading3Quarters /></div>
+            }
         </section>
     );
 }
